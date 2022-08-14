a726a45307c79689679f7e908e96b74a205d20e2
SalTMongoose <acotterson@users.noreply.github.com>
2022-08-14 01:50:24 -0600

    comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,32 @@
-var currentDay = moment().format("dddd - MMMM Do, YYYY"); 
-var currentHour =parseInt(moment().subtract(10, 'hours').format("H"));
+var currentDay = moment().format("dddd - MMMM Do, YYYY");
+var currentHour = parseInt(moment().subtract(10, "hours").format("H"));
+// regex to select all white space
 var spaces = /\s+/g;
 
-$('#currentDay').html(currentDay);
+$("#currentDay").html(currentDay);
 
+// loop through each hour and add colors and tasks
 $(".hour").each(function (index) {
   var currentDivHour = $(this).html().replace(spaces, "");
+  //   switch to military time
   var currentDivHourInt = parseInt(moment(currentDivHour, "hA").format("H"));
 
+  //   add colors depending on past, present, future
   if (currentDivHourInt === currentHour) {
-    $(this).siblings('textarea').addClass("present");
+    $(this).siblings("textarea").addClass("present");
   } else if (currentDivHourInt < currentHour) {
-    $(this).siblings('textarea').addClass("past");
+    $(this).siblings("textarea").addClass("past");
   } else {
-    $(this).siblings('textarea').addClass("future");
-  }  
-  
-  $(this).siblings('textarea').val(localStorage.getItem(currentDivHour));
+    $(this).siblings("textarea").addClass("future");
+  }
+  // load saved tasks
+  $(this).siblings("textarea").val(localStorage.getItem(currentDivHour));
 });
 
-$( "button" ).click(function() {
-    localStorage.setItem($(this).siblings('div').html().replace(spaces, ""),$(this).siblings('textarea').val());
+// save tasks on save button click
+$("button").click(function () {
+  localStorage.setItem(
+    $(this).siblings("div").html().replace(spaces, ""),
+    $(this).siblings("textarea").val()
+  );
 });
